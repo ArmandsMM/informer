826b17a3a2229d201472dc5dd79c3e7dd8d88f49
Armands <armands.mikanovskis@accenture.com>
2016-07-18 15:28:58 +0300

    initial commit

---
+++ AdebtlyAdaptiveChallenge/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using AdebtlyAdaptiveChallenge.model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdebtlyAdaptiveChallenge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<NewsItem> newsItemList;
+        private ObservableCollection<NewsItem> financialnewsItemList;
+        private ObservableCollection<NewsItem> foodnewsItemList;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+ 
+            newsItemList = NewsItemManager.GetNewsItems();
+            financialnewsItemList = new ObservableCollection<NewsItem>( newsItemList.Where(p => p.Category == "Financial").ToList());
+            foodnewsItemList =      new ObservableCollection<NewsItem>( newsItemList.Where(p => p.Category == "Food").ToList());
+
+            hamburgerListView.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            hamburgerSplitview.IsPaneOpen = !hamburgerSplitview.IsPaneOpen;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = (ListView)sender;
+            
+            if (selected.SelectedIndex == 0)
+            {
+                pageTitleText.Text = "Financial";
+                ContentGridView.ItemsSource = financialnewsItemList;
+            }
+            else if (selected.SelectedIndex == 1)
+            {
+                pageTitleText.Text = "Food";
+                ContentGridView.ItemsSource = foodnewsItemList;
+            }
+        }
+
+        private void ContentGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedItem = (NewsItem) e.ClickedItem;
+            if (selectedItem.Category == "Financial")
+            {
+                financialnewsItemList.Remove(selectedItem);
+            }
+            else if (selectedItem.Category == "Food")
+            {
+                foodnewsItemList.Remove(selectedItem);
+            }
+
+            newsItemList.Remove(selectedItem);
+        }
+    }
+}
---
+++ AdebtlyAdaptiveChallenge/model/NewsItemManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdebtlyAdaptiveChallenge.model
+{
+    class NewsItemManager
+    {
+        public static List<NewsItem> GetNewsItems()
+        {
+            var items = new List<NewsItem>();
+
+            items.Add(new NewsItem { Id = 6, Category = "Financial", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Financial1.png" });
+            items.Add(new NewsItem { Id = 7, Category = "Financial", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Financial2.png" });
+            items.Add(new NewsItem { Id = 8, Category = "Financial", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Financial3.png" });
+            items.Add(new NewsItem { Id = 9, Category = "Financial", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Financial4.png" });
+            items.Add(new NewsItem { Id = 10, Category = "Financial", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Financial5.png" });
+
+            items.Add(new NewsItem { Id = 1, Category = "Food", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Food1.png" });
+            items.Add(new NewsItem { Id = 2, Category = "Food", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Food2.png" });
+            items.Add(new NewsItem { Id = 3, Category = "Food", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Food3.png" });
+            items.Add(new NewsItem { Id = 4, Category = "Food", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Food4.png" });
+            items.Add(new NewsItem { Id = 5, Category = "Food", Headline = "Cool Headline text", Subhead = "cool subline text but smaller", DateLine = "10/12/2016", Image = "assets/Food5.png" });
+
+            return items;
+        }
+    }
+}
